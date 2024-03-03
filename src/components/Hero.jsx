c62ae2607634fd098d50c05953889ebb00c39f29
Genadi <genadi.kozarev@gmail.com>
2024-03-03 17:50:18 +0000

    add a parallax effect and fix some positioning of the Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import Section from "./Section";
 import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
+
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
@@ -11,10 +17,10 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      <div className="container relative mt-16">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mg:[6rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of Chatting with &nbsp;
+            Explore the Possibilities of&nbsp;Chatting with {` `}
             <span className="inline-block relative">
               AI Frequency
               <img
@@ -47,8 +53,20 @@ const Hero = () => {
                   height={490}
                   alt="Image of a white robot with glowing orange eyes"
                 />
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
               </div>
             </div>
+
+            <Gradient />
           </div>
           <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
@@ -59,6 +77,7 @@ const Hero = () => {
               alt="Image of a white robot with glowing orange eyes"
             />
           </div>
+          <BackgroundCircles />
         </div>
       </div>
     </Section>
