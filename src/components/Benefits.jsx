397140559ac0eebe3c04277898e36295b54cf575
Genadi <genadi.kozarev@gmail.com>
2024-03-04 10:01:51 +0000

    create Benefits section and start populating it with a Header

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,17 @@
+import Heading from "./Heading";
+import Section from "./Section";
+
+const Benefits = () => {
+  return (
+    <Section id="features">
+      <div className="container relative z-2">
+        <Heading
+          title="Chat Smarter, Not Harder with AI Frequency"
+          className="md:max-w-md lg:max-w-2xl"
+        />
+      </div>
+    </Section>
+  );
+};
+
+export default Benefits;
